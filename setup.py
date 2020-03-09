9b7c0857225b0a8a260861ea44f8334c73c76631
Paul W. Egeler, M.S., GStat <32426645+pegeler@users.noreply.github.com>
2020-03-09 15:29:39 -0400

    formatting setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,28 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-setup(name="BiomarkerTranslatorAPI",
-      version="0.0",
-      author="Paul W. Egeler, M.S., GStat",
-      author_email="paul.egeler@spectrumhealth.org",
-      description="NCATS Biomarker Translator API",
-      url="https://github.com/SpectrumHealthResearch/BiomarkerTranslatorAPI",
-      license="GPL-3",
-      package_dir={"BiomarkerTranslatorAPI": "src"},
-      packages=find_packages(),
+setup(name='BiomarkerTranslatorAPI',
+      version='0.0',
+      author='Paul W. Egeler, M.S., GStat',
+      author_email='paul.egeler@spectrumhealth.org',
+      description='NCATS Biomarker Translator API',
+      url='https://github.com/SpectrumHealthResearch/BiomarkerTranslatorAPI',
+      license='GPL-3',
+      package_dir={'BiomarkerTranslatorAPI': 'src'},
+      packages=[
+          'BiomarkerTranslatorAPI',
+          'BiomarkerTranslatorAPI.resources',
+          'BiomarkerTranslatorAPI.common',
+      ],
       install_requires=[
           'flask',
           'flask_restful',
           'pymysql',
           'python-dotenv',
       ],
-      keywords=["biomarker", "translational medicine", "API"],
-      python_requires=">=3.5")
+      keywords=[
+          'biomarker', 
+          'translational medicine', 
+          'API',
+      ],
+      python_requires='>=3.5')
