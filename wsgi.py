ad1c307acd3b89547942dd73c2059680914d8a10
Paul W. Egeler, M.S., GStat <32426645+pegeler@users.noreply.github.com>
2020-03-09 21:53:20 -0400

    simplifying wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,18 +10,4 @@ closer to production.
 Reference:
     https://flask.palletsprojects.com/en/1.1.x/deploying/mod_wsgi/
 """
-from os.path import join
-from dotenv import load_dotenv
-
-# Set project directory here
-project_dir = ''
-
-# This is for the .env config file
-load_dotenv(dotenv_path=join(project_dir, '.env'))
-
-# This is for the virtualenv
-activate_this = join(project_dir, '/bin/activate_this.py')
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
 from BiomarkerTranslatorAPI.api import app as application
