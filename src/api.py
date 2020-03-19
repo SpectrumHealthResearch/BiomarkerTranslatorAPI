5f5808793a06844f211bd5d07af13bca99f6a746
pegeler <32426645+pegeler@users.noreply.github.com>
2020-03-18 23:21:35 -0400

    adding swagger ui

--- src/api.py
+++ src/api.py
@@ -5,6 +5,7 @@ Biomarker Translator API
 """
 from flask import Flask
 from flask_restful import Api
+from flasgger import Swagger
 from .resources.biomarker import Biomarker
 from .resources.disease   import Disease
 from .resources.drug      import Drug
@@ -13,8 +14,14 @@ from .resources.patient   import Patient
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SWAGGER'] = {
+    'title': 'Biomarker Translator API',
+    'doc_dir': './docs/',
+    'uiversion': 2,
+}
+swagger = Swagger(app, parse=True)
 
-api.add_resource(Biomarker, '/biomarker', '/biomarker/', '/biomarker/<string:name>')
-api.add_resource(Disease,   '/disease',   '/disease/',   '/disease/<string:name>')
-api.add_resource(Drug,      '/drug',      '/drug/',      '/drug/<string:name>')
-api.add_resource(Patient,   '/patient',   '/patient/',   '/patient/<string:patient_id>')
+api.add_resource(Biomarker, '/biomarker/<string:name>')
+api.add_resource(Disease,   '/disease/<string:name>')
+api.add_resource(Drug,      '/drug/<string:name>')
+api.add_resource(Patient,   '/patient/<string:patient_id>')
